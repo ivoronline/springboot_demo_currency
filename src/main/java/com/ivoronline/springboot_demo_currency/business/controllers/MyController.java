27357fded2ccdca55fee3dc5b0448e967096cf9a
igoreta <ivoronline@gmail.com>
2021-03-11 21:03:22 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_demo_currency/business/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_demo_currency/business/controllers/MyController.java
@@ -4,9 +4,6 @@ import com.ivoronline.springboot_demo_currency.business.repositories.CurrencyRep
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.BindException;
-import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,12 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-
 import javax.validation.constraints.NotBlank;
 import java.time.LocalDate;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class MyController {
@@ -101,22 +95,5 @@ public class MyController {
     return message;
 
   }
-
-  //==================================================================
-  // HANDLE EXCEPTIONS (it only catches first exception)
-  //==================================================================
-  @ResponseBody
-  @ResponseStatus(HttpStatus.BAD_REQUEST)
-  @ExceptionHandler(Exception.class)
-  public String handleMyExceptions(Exception exception) {
-
-    //GET EXCEPTION DETAILS
-    String message       = exception.getMessage();       //Required String parameter 'name' is not present
-
-    //RETURN MESSAGE
-    return message;
-
-  }
-
 
 }
