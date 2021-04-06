630a41b9d29fd2b4b56929e086600a00f8bbf6be
igoreta <ivoronline@gmail.com>
2021-04-06 14:58:57 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_demo_currency/logger/entities/Log.java
+++ src/main/java/com/ivoronline/springboot_demo_currency/logger/entities/Log.java
@@ -11,15 +11,15 @@ public class Log {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  public Integer id;
-  public String  protocol;
-  public String  serverName;
-  public String  method;
-  public String  servletPath;
-  public String  queryString;
-  public Integer serverPort;
-  public String  username;
-  public Integer status;
+  public Integer   id;
+  public String    protocol;
+  public String    serverName;
+  public String    method;
+  public String    servletPath;
+  public String    queryString;
+  public Integer   serverPort;
+  public String    username;
+  public Integer   status;
   public LocalDate date;
 
 }
